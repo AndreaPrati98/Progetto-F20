b43abb5e8d74da0b58c0473ef0e921b343ed39c7
gCass <guglielmo.cassini01@universitadipavia.it>
2020-04-27 15:34:49 +0200

    Aggiunto metodo vuoto getConstraints in Component

--- ConfigurazionePc/src/model/component/Component.java
+++ ConfigurazionePc/src/model/component/Component.java
@@ -1,6 +1,16 @@
 package model.component;
+
+import java.util.List;
+
+import model.component.constraint.Constraint;
+
 //IreneRipari
 public class Component {
+
+	public List<Constraint> getConstraints() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 	
 	
--- ConfigurazionePc/src/model/component/constraint/EqualsConstraint.java
+++ ConfigurazionePc/src/model/component/constraint/EqualsConstraint.java
@@ -3,22 +3,38 @@ package model.component.constraint;
 import java.util.List;
 
 import model.component.Component;
-
+//Cic
 public class EqualsConstraint implements Constraint {
 
-	public EqualsConstraint() {
-		// TODO Auto-generated constructor stub
+	private String name;
+	private String value;
+
+	public EqualsConstraint(String name, String value) {
+		super();
+		this.name = name;
+		this.value = value;
 	}
 
 	@Override
 	public String getNomeVincolo() {
 		// TODO Auto-generated method stub
-		return null;
+		return name;
 	}
 
 	@Override
 	public boolean checkList(List<Component> components) {
 		// TODO Auto-generated method stub
+		
+		for(Component comp : components) {
+			
+			List<Constraint> constraints = comp.getConstraints();
+			
+			
+			
+		}
+		
+		
+		
 		return false;
 	}
 
