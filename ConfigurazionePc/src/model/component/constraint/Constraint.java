fdb32f90a7f3a71535bd7bbc8d360fdeb7a44161
gCass <guglielmo.cassini01@universitadipavia.it>
2020-04-27 15:30:32 +0200

    Aggiunti metodi all'interfaccia Constraint

--- ConfigurazionePc/src/model/component/constraint/Constraint.java
+++ ConfigurazionePc/src/model/component/constraint/Constraint.java
@@ -1,5 +1,12 @@
 package model.component.constraint;
+import model.component.*;
+
+import java.util.List;
+
 //GC
 public interface Constraint {
 
+	String getNomeVincolo();
+	boolean checkList(List<Component> components);
+	
 }
