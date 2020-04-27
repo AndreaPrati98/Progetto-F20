12dcd413cd644d6ac311e14e44086799f9ef24cf
gCass <guglielmo.cassini01@universitadipavia.it>
2020-04-27 15:32:00 +0200

    Aggiunta classe EqualsConstraint

---
+++ ConfigurazionePc/src/model/component/constraint/EqualsConstraint.java
@@ -0,0 +1,25 @@
+package model.component.constraint;
+
+import java.util.List;
+
+import model.component.Component;
+
+public class EqualsConstraint implements Constraint {
+
+	public EqualsConstraint() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public String getNomeVincolo() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean checkList(List<Component> components) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
