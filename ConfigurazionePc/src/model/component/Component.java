48f6b78044347ba7c2e96cc1e1e89f949a9fa4a3
AlessandroCapici <alessandro.capici01@universitadipavia.it>
2020-04-27 17:09:37 +0200

    riempita classe component

--- ConfigurazionePc/src/model/component/Component.java
+++ ConfigurazionePc/src/model/component/Component.java
@@ -1,18 +1,58 @@
 package model.component;
 
 import java.util.List;
+import java.util.Map;
 
 import model.component.constraint.Constraint;
 
-//IreneRipari
+//Capici Alessandro
 public class Component {
 
-	public List<Constraint> getConstraints() {
-		// TODO Auto-generated method stub
-		return null;
+	private String typeComponent;
+	private List<Constraint> constraintList;
+	private Map<String, String> attributesMap;
+
+	public Component(String typeComponent, List<Constraint> constraintList, Map<String, String> attributesMap) {
+		this.setTypeComponent(typeComponent);
+		this.constraintList = constraintList;
+		this.attributesMap=attributesMap;
 	}
 
 	
 	
 	
+	public List<Constraint> getConstraints() {
+		// TODO Auto-generated method stub
+		return this.constraintList;
+	}
+
+
+
+
+	public Map<String, String> getAttributesMap() {
+		return attributesMap;
+	}
+
+
+
+
+	public boolean setAttributesMap(String key, String attributes) {
+		attributesMap.replace(key, attributes);
+		return true;
+	}
+
+
+
+
+	public String getTypeComponent() {
+		return typeComponent;
+	}
+
+
+
+
+	public void setTypeComponent(String typeComponent) {
+		this.typeComponent = typeComponent;
+	}
+
 }
