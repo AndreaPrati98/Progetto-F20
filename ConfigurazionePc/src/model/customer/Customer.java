03616f20ad8f330aa7da3af01a3b27eb3b9adbfe
AndreaPrati98 <62189267+AndreaPrati98@users.noreply.github.com>
2020-05-01 12:56:49 +0200

    Filled Customer class

--- ConfigurazionePc/src/model/customer/Customer.java
+++ ConfigurazionePc/src/model/customer/Customer.java
@@ -1,7 +1,31 @@
 package model.customer;
 
+import java.lang.module.Configuration;
+import java.util.ArrayList;
+import java.util.List;
+/**
+ * 
+ * @author Andrea
+ *
+ */
 public class Customer {
 
+	//public static int MAX_NUMBER_OF_CONFIGURATIONS = 5;
 	
+	//private String name;
+	private List<Configuration> configurationList;
+
+	public Customer() {
+		configurationList = new ArrayList<Configuration>();
+	}
+	
+	public List<Configuration> getConfigurationList() {
+		return configurationList;
+	}
+	
+	/*
+	 * I metodi di add e di remove configuration non li ho fatti perchè mi sembra abbia più senso che 
+	 * la responsabilità di farlo l'abbia system e non il cliente direttamente dalla propria classe, ne parliamo alla riunione
+	 */
 	
 }
