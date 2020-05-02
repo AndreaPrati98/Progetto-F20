f8f92e5367d2b4a962724940dd5ccb62eae3d198
StefanoButera <stefano.butera01@universitadipavia.it>
2020-05-02 09:20:09 +0200

    prova stefano

--- ConfigurazionePc/src/model/Tester.java
+++ ConfigurazionePc/src/model/Tester.java
@@ -3,6 +3,7 @@ package model;
 public class Tester {
 	public static void main(String[] args) {
 	
-		System.out.println("prova stefano ");
+		System.out.println("prova stefano "); // prova
+		
 	}
 }
