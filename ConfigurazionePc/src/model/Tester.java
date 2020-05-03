c792b0e225ca61410a94a9643c6a742c3ede0038
AndreaPrati98 <62189267+AndreaPrati98@users.noreply.github.com>
2020-05-03 15:33:39 +0200

    fixed bugs in TesterFrame

--- ConfigurazionePc/src/model/Tester.java
+++ ConfigurazionePc/src/model/Tester.java
@@ -1,20 +1,37 @@
 package model;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.JFrame;
 
 import view.TesterFrame;
+import view.TesterFrame.TypeList;
 
 public class Tester {
 	
 	public static void main(String[] args) {
 	
-		
 		TesterFrame pippo = new TesterFrame("Frame di test");
 		pippo.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		pippo.setVisible(true);
 		
-		
+		pippo.getAddComponentButton().addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				
+				pippo.addElementJList(TypeList.ADDED_COMPONENT_LIST, "Processore bellissimo");
+				System.out.println("fatto 1");
+				pippo.addElementJList(TypeList.NEW_COMPONENT_LIST, "Scheda madre bellissima");
+				System.out.println("fatto 2");
+				pippo.addElementJList(TypeList.NEW_COMPONENT_LIST, "Case RGB");
+				System.out.println("fatto 3");
+			}
+		});
 		
 	}
+	
+	
 
 }
--- ConfigurazionePc/src/view/TesterFrame.java
+++ ConfigurazionePc/src/view/TesterFrame.java
@@ -3,7 +3,6 @@ package view;
 import java.awt.BorderLayout;
 import java.awt.Container;
 import java.awt.Dimension;
-import java.awt.FlowLayout;
 import java.awt.Toolkit;
 import java.util.LinkedList;
 
@@ -15,6 +14,7 @@ import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JSplitPane;
 import javax.swing.ScrollPaneConstants;
+
 /**
  * 
  * @author Andrea
@@ -26,11 +26,11 @@ import javax.swing.ScrollPaneConstants;
 public class TesterFrame extends JFrame {
 
 	// si parte dal presupposto che sia unica la configurazione che stiamo creando
-	
-	public static enum typeList {
+
+	public static enum TypeList {
 		NEW_COMPONENT_LIST, ADDED_COMPONENT_LIST;
 	}
-	
+
 	private Container content;
 	private JPanel addingRemovingComponent;
 	private JPanel managingConfiguration;
@@ -44,38 +44,39 @@ public class TesterFrame extends JFrame {
 	// Queste due liste servono solo ad aggiornare le JList (non conosco altri modi)
 	private LinkedList<String> bufferComponent;
 	private LinkedList<String> bufferNewComponent;
-	
+
 	public TesterFrame(String title) {
 		super(title);
 		this.bufferComponent = new LinkedList<String>();
+		this.bufferNewComponent = new LinkedList<String>();
 		
 		this.initialPosition();
 		content = this.getContentPane();
-		
+
 		content.setLayout(new BorderLayout());
-		
+
 		addingRemovingComponent = new JPanel();
 		managingConfiguration = new JPanel();
-		
+
 		componentJList = new JList<String>();
 		chosenJList = new JList<String>();
-		
+
 		choiceOfComponents = this.createJSplitPane(this.createScrollPanel(componentJList), this.createScrollPanel(chosenJList));
 		addingRemovingComponent.add(choiceOfComponents);
-		
+
 		addComponentButton = new JButton("AGGIUNGI COMPONENTE");
 		removeComponenButtont = new JButton("RIMUOVI COMPONENTE");
 		addingRemovingComponent.add(addComponentButton);
 		addingRemovingComponent.add(removeComponenButtont);
-		
+
 		infoLabel = new JLabel("Qui visualizzerai le informazioni sulla configurazione che stai creando");
 		managingConfiguration.add(infoLabel);
-		
+
 		content.add(addingRemovingComponent, BorderLayout.NORTH);
 		content.add(managingConfiguration, BorderLayout.SOUTH);
-		
+
 	}
-	
+
 	/**
 	 * Create a JSplitPanel using two JScrollPane
 	 * 
@@ -87,22 +88,22 @@ public class TesterFrame extends JFrame {
 	 * @see JSplitPane
 	 */
 	private JSplitPane createJSplitPane(JScrollPane pannello1, JScrollPane pannello2) {
-		
-		//Create a split pane with the two scroll panes in it.
+
+		// Create a split pane with the two scroll panes in it.
 		JSplitPane splitPane = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, pannello1, pannello2);
 		splitPane.setOneTouchExpandable(true);
 		splitPane.setDividerLocation(150);
 
-		//Provide minimum sizes for the two components in the split pane
+		// Provide minimum sizes for the two components in the split pane
 		Dimension minimumSize = new Dimension(80, 50);
 		pannello1.setMinimumSize(minimumSize);
 		pannello2.setMinimumSize(minimumSize);
 		return splitPane;
-		
+
 	}
-	
+
 	/**
-	 * Make a ScrollablePanel with a JList 
+	 * Make a ScrollablePanel with a JList
 	 * 
 	 * @param listText
 	 * @return the ScrollPanel
@@ -111,73 +112,107 @@ public class TesterFrame extends JFrame {
 	 * @see JList
 	 */
 	private JScrollPane createScrollPanel(JList<String> listText) {
-		
+
 		JScrollPane scrollPane = new JScrollPane(listText);
 		scrollPane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED);
 		scrollPane.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_AS_NEEDED);
-		scrollPane.setPreferredSize(new Dimension(250, 180));		
+		scrollPane.setPreferredSize(new Dimension(250, 180));
 		return scrollPane;
-	
+
 	}
-	
+
 	private void initialPosition() {
-		
+
 		Toolkit kit = Toolkit.getDefaultToolkit();
 		Dimension screenSize = kit.getScreenSize();
 		int screenHeight = screenSize.height;
 		int screenWidth = screenSize.width;
-		setSize(screenWidth/2,screenHeight/2);
-		setLocation(screenWidth/4,screenHeight/4);
-		
+		setSize(screenWidth / 2, screenHeight / 2);
+		setLocation(screenWidth / 4, screenHeight / 4);
+
 	}
-	
+
 	/**
+	 * Add an element to a JList and refresh the JSplitPanel in which there is the list
 	 * 
 	 * @param s
-	 * @param typePanel - type of panel in witch there is the list we wanto to 
+	 * @param typePanel - type of panel in which there is the list we wanto to
 	 */
-	
-	
-	public void addElementJList(TesterFrame.typeList typeList, String s) {
-		//TODO  
-		bufferComponent.add(s);
-		//devo aggiungere il vettore di stringe, lo dichiaro, lo riempio e poi lo metto dento alla JList 
-		String[] listData = new String[bufferComponent.size()];
+	public void addElementJList(TesterFrame.TypeList typeList, String s) {
 		
-		int i = 0;
-		for(String elem : bufferComponent) {	
-			listData[i] = elem;
-			i++;
-			
-		}
-		
-		if(typeList == TesterFrame.typeList.NEW_COMPONENT_LIST) {
-			componentJList.setListData(listData);
-		}else if(typeList == TesterFrame.typeList.ADDED_COMPONENT_LIST) {
-			componentJList.setListData(listData);
+		// devo aggiungere il vettore di stringe, lo dichiaro, lo riempio e poi lo metto
+		// dento alla JList
+
+		String[] listData = null;
+		if (typeList == TesterFrame.TypeList.NEW_COMPONENT_LIST) {
+			bufferNewComponent.add(s);
+			listData = new String[bufferNewComponent.size()];
+			int i = 0;
+			for (String elem : bufferNewComponent) {
+				listData[i] = elem;
+				i++;
+			}
+		} else if (typeList == TesterFrame.TypeList.ADDED_COMPONENT_LIST) {
+			bufferComponent.add(s);
+			listData = new String[bufferComponent.size()];
+			int i = 0;
+			for (String elem : bufferComponent) {
+				listData[i] = elem;
+				i++;
+			}
 		} else {
 			throw new IllegalArgumentException("typeList this method isn't able to handle");
 		}
 		
+
+		// questa sezione decide quale JList aggiornare
+		if (typeList == TesterFrame.TypeList.NEW_COMPONENT_LIST) {
+			componentJList.setListData(listData);
+		} else if (typeList == TesterFrame.TypeList.ADDED_COMPONENT_LIST) {
+			chosenJList.setListData(listData);
+		} else {
+			throw new IllegalArgumentException("typeList this method isn't able to handle");
+		}
+
 		addingRemovingComponent.remove(choiceOfComponents);
 		choiceOfComponents = this.createJSplitPane(this.createScrollPanel(componentJList), this.createScrollPanel(chosenJList));
-		
+		addingRemovingComponent.add(choiceOfComponents);
+
 		addingRemovingComponent.revalidate();
 		addingRemovingComponent.repaint();
-/*		
-		scrollPaneIban = createScrollPanel(ibanList);
-		
-		//creo spazio per le info
-		infoConto = createJTextArea();
-		scrollPaneInfo = createScrollPanel(infoConto);
-		
-		//assemblo il JSplitPanel e lo aggiungo al pannello
-		splitPane = createJSplitPane(scrollPaneIban, scrollPaneInfo);
-		p0.add(splitPane);
-		
-		p0.revalidate();
-		p0.repaint();
-*/		
+
 	}
-	
+
+	public JPanel getAddingRemovingComponent() {
+		return addingRemovingComponent;
+	}
+
+	public JPanel getManagingConfiguration() {
+		return managingConfiguration;
+	}
+
+	public JSplitPane getChoiceOfComponents() {
+		return choiceOfComponents;
+	}
+
+	public JButton getAddComponentButton() {
+		return addComponentButton;
+	}
+
+	public JButton getRemoveComponenButtont() {
+		return removeComponenButtont;
+	}
+
+	public JLabel getInfoLabel() {
+		return infoLabel;
+	}
+
+	public JList<String> getComponentJList() {
+		return componentJList;
+	}
+
+	public JList<String> getChosenJList() {
+		return chosenJList;
+	}
+
 }
