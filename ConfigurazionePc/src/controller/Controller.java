f8986ac02a8a7b8297db29a56f495317dfc1111a
AlessandroCapici <alessandro.capici01@universitadipavia.it>
2020-05-04 13:16:32 +0200

    visulizzazione di tutti i parametri dei comp

--- ConfigurazionePc/src/controller/Controller.java
+++ ConfigurazionePc/src/controller/Controller.java
@@ -3,9 +3,11 @@ package controller;
 import model.component.Component;
 import model.configurator.Configurator;
 import view.TesterFrame;
+
 /**
  * 
  * @author Irene Ripari
+ * @author Capici Alessandro
  *
  */
 public class Controller {
@@ -21,15 +23,21 @@ public class Controller {
 	public Controller(TesterFrame view, Configurator model) {
 		this.view = view;
 		this.model = model;
-		String s=null;
+		String s = null;
 		for (Component c : model.getCatalog().getComponentList()) {
-			s=c.getAttributesMap().get("name");
+			for (String keylist : c.getAttributesMap().keySet()) {
+				s = c.getTypeComponent()+"    name : " + c.getAttributesMap().get("name") + "    ";
+				if (c.getAttributesMap().get(keylist) != null && !keylist.equals("name")) {
+					s = s + keylist + " : " + c.getAttributesMap().get(keylist) + "    ";
+				}
+			}
 			view.getListModelCatalog().addElement(s);
+			s = "";
 		}
 		addListener();
-		
+
 	}
-	
+
 	private void addListener() {
 		view.getAddComponentButton().addActionListener(new Listener(view, model));
 		view.getRemoveComponenButtont().addActionListener(new Listener(view, model));
