7ca3bff4db5e71bd2647ef80df6579821c4039bd
AlessandroCapici <alessandro.capici01@universitadipavia.it>
2020-05-04 13:24:08 +0200

    path

--- ConfigurazionePc/src/model/util/JSONUtil.java
+++ ConfigurazionePc/src/model/util/JSONUtil.java
@@ -125,7 +125,7 @@ public class JSONUtil {
 		constraintList = new ArrayList<Constraint>();
 		map = new HashMap<>();
 		in = new Scanner(System.in);
-		JSON_PATH = "ContenitoreFile/components.json";
+		JSON_PATH = "ConfigurazionePC/ContenitoreFile/components.json";
 	}
 
 	public void addComponent() {
