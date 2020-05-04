67ca6b48978e6cdb0c06783efffbc6157e77cc48
AlessandroCapici <alessandro.capici01@universitadipavia.it>
2020-05-04 17:51:20 +0200

    eliminazione commento

--- src/controller/Tester.java
+++ src/controller/Tester.java
@@ -5,31 +5,13 @@ import model.configurator.Configurator;
 import view.TesterFrame;
 
 public class Tester {
-	
+
 	public static void main(String[] args) {
 
-		ComponentCatalog catalog=new ComponentCatalog();
-		Configurator model=new Configurator(catalog);
-		TesterFrame view=new TesterFrame("Prova");
-		Controller controller=new Controller(view, model);
-		
-//		TesterFrame pippo = new TesterFrame("Frame di test");
-//		pippo.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-//		pippo.setVisible(true);
-//		
-//		pippo.getAddComponentButton().addActionListener(new ActionListener() {
-//			
-//			@Override
-//			public void actionPerformed(ActionEvent e) {
-//				
-//				pippo.addElementJList(TypeList.ADDED_COMPONENT_LIST, "Processore bellissimo");
-//				System.out.println("fatto 1");
-//				pippo.addElementJList(TypeList.NEW_COMPONENT_LIST, "Scheda madre bellissima");
-//				System.out.println("fatto 2");
-//				pippo.addElementJList(TypeList.NEW_COMPONENT_LIST, "Case RGB");
-//				System.out.println("fatto 3");
-//			}
-//		});
+		ComponentCatalog catalog = new ComponentCatalog();
+		Configurator model = new Configurator(catalog);
+		TesterFrame view = new TesterFrame("Prova");
+		Controller controller = new Controller(view, model);
 
 	}
 
