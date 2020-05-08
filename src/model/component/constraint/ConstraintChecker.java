36eb74e6e59521580e14ee33abeada8754f7c2dc
gCass <guglielmo.cassini01@universitadipavia.it>
2020-05-08 15:54:28 +0200

    Aggiunto documentazione a ConstraintChecker

--- src/model/component/constraint/ConstraintChecker.java
+++ src/model/component/constraint/ConstraintChecker.java
@@ -5,15 +5,23 @@ import java.util.List;
 
 import model.component.Component;
 /**
- * @author Guglielmo 
- * @author Irene S.
- *
+ * @author Guglielmo Cassini
+ * @author Irene Schimperna
+ * 
+ * The ConstraintChekcker is the object that extrapolates the Constraints from a list of
+ * given components that are already compatible each others and check if the constraints 
+ * from a new component are compatible with the list.
+ * 
  */
 public class ConstraintChecker implements InterfaceConstraintChecker {
 	
 	public ConstraintChecker() {
 	}
-	
+	/**
+	 * 
+	 * @return List<Contraints> the list of the Constraints that are not respected from
+	 * the new component with the previous components.
+	 */
 	@Override
 	public List<Constraint> check(Component newComponent, List<Component> oldComponents) {
 
