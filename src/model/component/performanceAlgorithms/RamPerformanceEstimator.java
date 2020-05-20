d682a9192e00b4984eaa6c2bee25dc7d77370e14
AndreaPrati98 <62189267+AndreaPrati98@users.noreply.github.com>
2020-05-20 16:27:48 +0200

    Generate classi Estimator

--- src/model/component/performanceAlgorithms/RamPerformanceEstimator.java
+++ src/model/component/performanceAlgorithms/RamPerformanceEstimator.java
@@ -2,13 +2,11 @@ package model.component.performanceAlgorithms;
 
 import java.util.Map;
 
-public class CPUPerformanceEstimator implements InterfacePerformanceEstimator {
+public class RamPerformanceEstimator implements InterfacePerformanceEstimator {
 
 	@Override
 	public double computePerformance(Map<String, String> componentAttributes) {
-		
-		
-		
+		// TODO Auto-generated method stub
 		return 0;
 	}
 
