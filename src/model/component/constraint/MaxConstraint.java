6b93be993ad6db6c590835a4b72fba77e3f71504
gCass <guglielmo.cassini01@universitadipavia.it>
2020-05-25 23:53:56 +0200

    Eliminato vecchio metodo definitivamente

--- src/model/component/constraint/MaxConstraint.java
+++ src/model/component/constraint/MaxConstraint.java
@@ -25,68 +25,6 @@ public class MaxConstraint extends AbstractConstraint {
 	 * @param Constraint type:{@link Constraint}
 	 * @return true if the component will respect the constraint,false if it will not respect the costraint
 	 */	
-	/*@Override
-	public boolean checkList(List<Constraint> constraints) {
-		
-		String myName = this.name;
-		double myValue = Double.parseDouble(this.value);
-		double sum = 0;
-		double max;
-		//Prima controllo se il vincolo è interno o esterno
-		
-		if(this.constraintType == ConstraintType.EXTERNAL) {
-			max = myValue;
-			for(Constraint constr : constraints) {
-				if(constr.getConstraintName().equals(myName) && constr.getConstraintType() == ConstraintType.INTERNAL) {
-					sum = sum + Double.parseDouble(constr.getValue());
-				}else if(constr.getConstraintName().equals(myName) && constr.getConstraintType() == ConstraintType.EXTERNAL) {
-					//Se ci sono due vincoli esterni con lo stesso nome,
-					//si tiene quello con il valore minore
-					double hisMax = Double.parseDouble(constr.getValue());
-					if(hisMax < max)
-						max = hisMax;
-				}
-			}			
-			
-			if(max < sum) {
-				return false;
-			}
-			
-			return true;
-		}else {
-			//Il vincolo è interno
-			max = 0;
-			boolean firstTime = true;
-			sum = sum + myValue;
-			for(Constraint constr : constraints) {
-				if(constr.getConstraintName().equals(myName)) {
-					if(constr.getConstraintType() == ConstraintType.EXTERNAL){
-						if(firstTime) {
-							max = Double.parseDouble(constr.getValue());
-							firstTime = false;
-						}else if(max > Double.parseDouble(constr.getValue())) {
-							max = Double.parseDouble(constr.getValue());
-						}						
-					} else {	
-						//if constr is internal
-						sum = sum + Double.parseDouble(constr.getValue());
-					}
-				}
-			}
-			
-			if(max == 0)
-				return true;
-			
-			if(max < sum) {
-				return false;
-			}
-			
-			return true;
-		}		
-	}*/
-	
-	
-
 	@Override
 	public boolean checkList(List<Component> oldCheckedComponents, Component componentToCheck) {
 		List<Attribute> oldAttributesAlreadyChecked = this.selectAttributeSameName(oldCheckedComponents);
