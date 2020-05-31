986f96240ead2fc73ebc5d5074998c00adde128e
AlessandroCapici <alessandro.capici01@universitadipavia.it>
2020-05-31 22:30:26 +0200

    mail

--- src/statics/Mail.java
+++ src/statics/Mail.java
@@ -10,31 +10,29 @@ import javax.mail.Session;
 import javax.mail.Transport;
 import javax.mail.internet.AddressException;
 import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeBodyPart;
 import javax.mail.internet.MimeMessage;
+
 @SuppressWarnings("unused")
 public class Mail {
 
-	private String from;
-
-	private String to;
-	private String host;
-	private String object;
+	private static final String from = "confpcf20@gmail.com";
+	private static final String password = "prendiamocisto30";
+	private static final String host = "smtp.gmail.com";
+	private String object = "Registrazione avvenuta";
 	private String text;
-	private String password;
+	private String to;
 
 	public Mail(String to) {
-		this.from = "confpcf20@gmail.com";
-		this.password="prendiamocisto30";
+
 		this.to = to;
-		this.host = "smtp.gmail.com";
-		this.object = "Registrazione avvenuta";
 		this.text = "Benvenuto nel nostro sito";
 		Properties prop = System.getProperties();
-		prop.put("mail.smtp.host", "smtp.gmail.com");
-        prop.put("mail.smtp.port", "465");
-        prop.put("mail.smtp.auth", "true");
-        prop.put("mail.smtp.socketFactory.port", "465");
-        prop.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
+		prop.put("mail.smtp.host", host);
+		prop.put("mail.smtp.port", "465");
+		prop.put("mail.smtp.auth", "true");
+		prop.put("mail.smtp.socketFactory.port", "465");
+		prop.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
 		Session session = Session.getInstance(prop, new javax.mail.Authenticator() {
 			protected PasswordAuthentication getPasswordAuthentication() {
 				return new PasswordAuthentication(from, password);
@@ -57,5 +55,18 @@ public class Mail {
 		}
 
 	}
+	
+	private MimeMessage testoHtml() throws AddressException, MessagingException {
+		MimeMessage msg = null;
+		msg.setFrom(new InternetAddress(from));
+		msg.addRecipients(Message.RecipientType.TO, InternetAddress.parse(to, false));
+		msg.setSubject(object);
+		msg.setText(text);
+		msg.setSentDate(new Date());
+		MimeBodyPart textPart = new MimeBodyPart();
+		//textPart.setText("<html><he
+		return msg;
+	}
+	
 
 }
