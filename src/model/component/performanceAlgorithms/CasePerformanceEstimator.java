de0ebebb9bd80fb43d7ffa5bf8d8e5eb957ed121
AndreaPrati98 <62189267+AndreaPrati98@users.noreply.github.com>
2020-06-02 22:13:23 +0200

    riempito case perf estimator

--- src/model/component/performanceAlgorithms/CasePerformanceEstimator.java
+++ src/model/component/performanceAlgorithms/CasePerformanceEstimator.java
@@ -6,10 +6,35 @@ import model.component.Attribute;
 
 public class CasePerformanceEstimator implements InterfacePerformanceEstimator {
 
+	private static final double MAX_POINT_FORM_FACTOR = 100;
+	
 	@Override
 	public double computePerformance(Map<String, Attribute> componentAttributes) {
-		// TODO Auto-generated method stub
-		return 0;
+		
+		double index = 0;
+		
+		if(componentAttributes == null) {
+			throw new NullPointerException("Invalid instance of componentAttributes");
+		}
+		
+		/*
+		 * I case hanno 2 standard attribute al momento, solo uno di questi interessa le performance, volendo
+		 */
+		
+		Attribute att = componentAttributes.get("formFactor");
+		String formFactor = att.getValue();
+		
+		if(formFactor == "atx") {
+			index = CasePerformanceEstimator.MAX_POINT_FORM_FACTOR;			
+		} else if (formFactor == "micro-atx") {
+			index = CasePerformanceEstimator.MAX_POINT_FORM_FACTOR * 2/3;			
+		} else if (formFactor == "mini-atx") {
+			index = CasePerformanceEstimator.MAX_POINT_FORM_FACTOR /3;
+		} else if(formFactor == null) {
+			index = -1;
+		}
+		
+		return index;
 	}
 
 }
