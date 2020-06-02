c281801452ae6ead19ef0195c7b0511ce31696ef
AlessandroCapici <alessandro.capici01@universitadipavia.it>
2020-06-02 22:26:03 +0200

    aggiunte tuplr per db

--- src/model/dao/InterfaceCostraintDAO.java
+++ src/model/dao/InterfaceCostraintDAO.java
@@ -6,7 +6,6 @@ import model.component.constraint.AbstractConstraint;
 
 public interface InterfaceCostraintDAO {
 	
-	//public AbstractConstraint getConstraint(String name, String typeOfComponent);
 	public List<AbstractConstraint> getAllConstraints(); 
 	public boolean addNewConstraint(String name, String type);
 	public boolean removeConstraint(String name);
