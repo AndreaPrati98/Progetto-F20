f325346835d29082dc9ae148f5c19b210ce4dba1
AlessandroCapici <alessandro.capici01@universitadipavia.it>
2020-06-04 21:49:25 +0200

    fix che non servono a nulla

--- src/controller/Tester.java
+++ src/controller/Tester.java
@@ -20,7 +20,7 @@ public class Tester {
 		servlet.add(new HomeServlet("home","/"));
 		servlet.add(new RegisterServlet("register", "/sign-in"));
 		servlet.add(new ConfigurationServlet("configuration", "/configuration/*"));
-		new ApplicationServer(8080, servlet).start();
+		new ApplicationServer(8081, servlet).start();
 		
 		// Questo va fatto all'avvio per inizializzare gli id da usare per le configurazioni
 		PersistenceFacade pf = PersistenceFacade.getIstance();
