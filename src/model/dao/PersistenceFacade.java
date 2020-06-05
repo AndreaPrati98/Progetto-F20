2598881e7780ab63222fcc504a2938f7e25cbb57
StefanoButera <stefano.butera01@universitadipavia.it>
2020-06-06 00:23:26 +0200

    errore corretto

--- src/model/dao/PersistenceFacade.java
+++ src/model/dao/PersistenceFacade.java
@@ -49,9 +49,9 @@ public class PersistenceFacade {
 
 	}
 
-	public boolean addConfiguration(Configuration conf,Object object) {
+	public boolean addConfiguration(Configuration conf,Customer user) {
 
-		return confdao.addConfiguration(conf,object);
+		return confdao.addConfiguration(conf,user);
 
 	}
 	
