b0e68fcf2f484474cca0198daf7e9f5889baed28
AndreaPrati98 <62189267+AndreaPrati98@users.noreply.github.com>
2020-06-05 23:13:14 +0200

    creato una classe di testing

---
+++ src/model/testSeri/ConstraintTester.java
@@ -0,0 +1,19 @@
+package model.testSeri;
+
+import static org.junit.Assert.assertEquals;
+
+public class ConstraintTester {
+
+	private int res;
+	
+	public ConstraintTester() {
+		assertEquals("ttapposto", 2, 1+1);
+
+	}
+	
+		
+		
+		
+}
+	
+
---
+++ src/model/testSeri/mainJunitTester.java
@@ -0,0 +1,12 @@
+package model.testSeri;
+
+public class mainJunitTester {
+
+	public static void main(String[] args) {
+	
+		ConstraintTester c = new ConstraintTester();
+		
+		
+	}
+
+}
