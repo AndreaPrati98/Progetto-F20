f000e714c08b36e6737d0b6ed853abf04f39e2e7
AndreaPrati98 <62189267+AndreaPrati98@users.noreply.github.com>
2020-06-05 22:29:23 +0200

    faccio branch per provare a usare junit

---
+++ src/model/testSeri/Prova_JUnit.java
@@ -0,0 +1,21 @@
+package model.testSeri;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class Prova_JUnit {
+
+	private int res;
+	
+	public Prova_JUnit() {
+		
+	}
+	
+	@Test
+	public void addition() {
+		
+		
+		
+	}
+	
+}
