1ec3e8245246894d886813846153d30944f56d5e
AndreaPrati98 <62189267+AndreaPrati98@users.noreply.github.com>
2020-06-06 15:51:10 +0200

    aggiunti esperimenti JUnit

---
+++ src/model/testSeri/ConfiguratorTester.java
@@ -0,0 +1,14 @@
+package model.testSeri;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ConfiguratorTester {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ src/model/testSeri/MaxConstraintTest.java
@@ -0,0 +1,34 @@
+package model.testSeri;
+
+import static org.junit.Assert.fail;
+
+import org.junit.Test;
+
+public class MaxConstraintTest {
+
+	@Test
+	void testCheckList() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testMaxConstraint() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSelectAttributeSameNameListOfComponent() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSelectAttributeSameNameComponent() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testFilterAttributesList() {
+		fail("Not yet implemented");
+	}
+
+}
