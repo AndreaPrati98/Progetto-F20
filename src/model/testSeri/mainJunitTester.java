e0b04d7e063e3a7d7102e439f9a176f0132d97d8
AndreaPrati98 <62189267+AndreaPrati98@users.noreply.github.com>
2020-06-06 15:50:48 +0200

    aggiunta possibilità di runnare test come java app

--- src/model/testSeri/mainJunitTester.java
+++ src/model/testSeri/mainJunitTester.java
@@ -1,11 +1,17 @@
 package model.testSeri;
 
+import org.junit.runner.JUnitCore;
+import org.junit.runner.Result;
+import org.junit.runner.notification.Failure;
+
 public class mainJunitTester {
 
 	public static void main(String[] args) {
 	
-		ConstraintTester c = new ConstraintTester();
-		
+		Result result = JUnitCore.runClasses(MaxConstraintTest.class);
+		for (Failure elem : result.getFailures()) {
+			System.out.println(elem.toString());
+		}
 		
 	}
 
