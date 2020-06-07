70513d1687545e889e34e97f6b4dc74dc7f71757
AlessandroCapici <alessandro.capici01@universitadipavia.it>
2020-06-08 01:12:22 +0200

    piccola modifica login

--- src/servlet/LoginServlet.java
+++ src/servlet/LoginServlet.java
@@ -1,41 +1,53 @@
 package servlet;
 
 import java.io.IOException;
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
 import org.rythmengine.Rythm;
 
 import controller.ServletController;
 
-public class LoginServlet extends MyServlet{
+public class LoginServlet extends MyServlet {
 
 	public LoginServlet(String name, String path) {
 		super(name, path);
 	}
-	
+
 	@Override
 	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		response.getWriter().write(Rythm.render("login.html"));
 	}
-	
+
 	@Override
-	public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {		
+	public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		String email = request.getParameter("Email");
 		String password = request.getParameter("Password");
-		
-		ServletController controller = ServletController.getIstance();
-		if(controller.login(email, password)) {
-			request.getSession().setAttribute("email", email);
-			response.sendRedirect("/configuration");		
+		MessageDigest digest;
+
+		try {
+			digest = MessageDigest.getInstance("SHA-256");
+			digest.reset();
+			digest.update(password.getBytes("utf8"));
+			password = String.format("%064x", new BigInteger(1, digest.digest()));
+		} catch (NoSuchAlgorithmException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
-		else {
+
+		ServletController controller = ServletController.getIstance();
+		if (controller.login(email, password)) {
+			request.getSession().setAttribute("email", email);
+			response.sendRedirect("/configuration");
+		} else {
 			response.sendRedirect("/login");
 		}
-		
+
 	}
 
 }
