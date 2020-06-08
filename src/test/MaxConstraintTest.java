a9f65f3a28107f8db60efa3fd15d7aa4fc057d9d
AndreaPrati98 <62189267+AndreaPrati98@users.noreply.github.com>
2020-06-08 14:42:01 +0200

    aggiunto esperimento di test

--- src/test/MaxConstraintTest.java
+++ src/test/MaxConstraintTest.java
@@ -1,45 +1,67 @@
 package test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.Assert.assertTrue;
 import static org.junit.jupiter.api.Assertions.fail;
 
+import org.junit.Ignore;
 import org.junit.Test; // importante che sia org.junit.Test
+
+import model.component.constraint.MaxConstraint;
+
+/**
+ * 
+ * @author Andrea
+ *
+ */
 
 
 public class MaxConstraintTest {
 	
 	@Test
+	public void testConstructor() {
+		MaxConstraint maxCon = new MaxConstraint("Prova");
+		assertTrue("Il costruttore non ha fuznionato", maxCon != null);
+	}
+	
+	@Test
+	@Ignore
 	public void testCheckList() {
-		assertEquals("ciao", "ciao");
+		fail("Not yet implemented");
 		
 	}
 
 	@Test
+	@Ignore
 	public void testMaxConstraint() {
 		fail("Not yet implemented");
 	}
 
 	@Test
+	@Ignore
 	public void testAbstractConstraint() {
 		fail("Not yet implemented");
 	}
 
 	@Test
+	@Ignore
 	public void testGetConstraintName() {
 		fail("Not yet implemented");
 	}
 
 	@Test
+	@Ignore
 	public void testSelectAttributeSameNameListOfComponent() {
 		fail("Not yet implemented");
 	}
 
 	@Test
+	@Ignore
 	public void testSelectAttributeSameNameComponent() {
 		fail("Not yet implemented");
 	}
 
 	@Test
+	@Ignore
 	public void testFilterAttributesList() {
 		fail("Not yet implemented");
 	}
