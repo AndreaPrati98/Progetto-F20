839b190690dfebd2d3dc5ca0b19355ae1c1c7370
AndreaPrati98 <62189267+AndreaPrati98@users.noreply.github.com>
2020-06-08 12:19:14 +0200

    resa public la classe e i metodi

--- src/test/MaxConstraintTest.java
+++ src/test/MaxConstraintTest.java
@@ -4,40 +4,40 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
-class MaxConstraintTest {
+public class MaxConstraintTest {
 
 	@Test
-	void testCheckList() {
+	public void testCheckList() {
 		fail("Not yet implemented");
 	}
 
 	@Test
-	void testMaxConstraint() {
+	public void testMaxConstraint() {
 		fail("Not yet implemented");
 	}
 
 	@Test
-	void testAbstractConstraint() {
+	public void testAbstractConstraint() {
 		fail("Not yet implemented");
 	}
 
 	@Test
-	void testGetConstraintName() {
+	public void testGetConstraintName() {
 		fail("Not yet implemented");
 	}
 
 	@Test
-	void testSelectAttributeSameNameListOfComponent() {
+	public void testSelectAttributeSameNameListOfComponent() {
 		fail("Not yet implemented");
 	}
 
 	@Test
-	void testSelectAttributeSameNameComponent() {
+	public void testSelectAttributeSameNameComponent() {
 		fail("Not yet implemented");
 	}
 
 	@Test
-	void testFilterAttributesList() {
+	public void testFilterAttributesList() {
 		fail("Not yet implemented");
 	}
 
