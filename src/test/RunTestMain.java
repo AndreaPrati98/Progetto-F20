6d294522a5231478fef8444c6a9cc31ad1562296
AndreaPrati98 <62189267+AndreaPrati98@users.noreply.github.com>
2020-06-08 14:42:48 +0200

    impostate le syso per il RunTestMain

--- src/test/RunTestMain.java
+++ src/test/RunTestMain.java
@@ -4,6 +4,11 @@ import org.junit.runner.JUnitCore;
 import org.junit.runner.Result;
 import org.junit.runner.notification.Failure;
 
+/**
+ * 
+ * @author Andrea
+ *
+ */
 public class RunTestMain {
 
 	public static void main(String[] args) {
@@ -13,6 +18,10 @@ public class RunTestMain {
 			System.out.println(failure.toString());
 		}
 		
+		System.out.println("Success: "+ (res.getRunCount() - res.getFailureCount()));
+		System.out.println("Failures: "+ res.getFailureCount());
+		System.out.println("Number of runned tests: "+ res.getRunCount());
+		System.out.println("Number of ignored test: "+ res.getIgnoreCount());
 		System.out.println("Was successful? "+ res.wasSuccessful());
 		
 	}
