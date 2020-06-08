80ae46fcfb3ad7e39c41192fbc4080ac2c6ea6ed
AndreaPrati98 <62189267+AndreaPrati98@users.noreply.github.com>
2020-06-08 15:33:27 +0200

    aggiunte altri test al MaxConstraintTest

--- src/test/MaxConstraintTest.java
+++ src/test/MaxConstraintTest.java
@@ -1,6 +1,8 @@
 package test;
 
+import static org.junit.Assert.assertSame;
 import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.fail;
 
 import org.junit.Ignore;
@@ -20,7 +22,16 @@ public class MaxConstraintTest {
 	@Test
 	public void testConstructor() {
 		MaxConstraint maxCon = new MaxConstraint("Prova");
+		MaxConstraint expected = new MaxConstraint("Prova");
 		assertTrue("Il costruttore non ha fuznionato", maxCon != null);
+		assertSame("I due oggetti non puntano alla stessa referance", expected, maxCon);
+	}
+	
+	@Test
+	public void testEquals() {
+		MaxConstraint maxCon = new MaxConstraint("Prova");
+		MaxConstraint expected = new MaxConstraint("Prova");
+		assertEquals(expected, maxCon, "I due oggetti non sono uguali");
 	}
 	
 	@Test
