cbd29b658d050be7f596ef68d6ab9dd72af823cb
AndreaPrati98 <62189267+AndreaPrati98@users.noreply.github.com>
2020-06-08 12:06:16 +0200

    aggiunto package dedicato ai test

---
+++ src/test/MaxConstraintTest.java
@@ -0,0 +1,44 @@
+package test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class MaxConstraintTest {
+
+	@Test
+	void testCheckList() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testMaxConstraint() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testAbstractConstraint() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetConstraintName() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSelectAttributeSameNameListOfComponent() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSelectAttributeSameNameComponent() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testFilterAttributesList() {
+		fail("Not yet implemented");
+	}
+
+}
