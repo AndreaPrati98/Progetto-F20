50cd20cd1fa1ccb267b2a5f5b19822fb2ca180cf
AndreaPrati98 <62189267+AndreaPrati98@users.noreply.github.com>
2020-06-17 21:49:07 +0200

    tolti dei commenti

--- src/test/RunTestMain.java
+++ src/test/RunTestMain.java
@@ -12,18 +12,7 @@ import org.junit.runner.notification.Failure;
 public class RunTestMain {
 
 	public static void main(String[] args) {
-/*		Result res = JUnitCore.runClasses(MaxConstraintTest.class);
 		
-		for (Failure failure : res.getFailures()) {
-			System.out.println(failure.toString());
-		}
-		
-		System.out.println("Success: "+ (res.getRunCount() - res.getFailureCount()));
-		System.out.println("Failures: "+ res.getFailureCount());
-		System.out.println("Number of runned tests: "+ res.getRunCount());
-		System.out.println("Number of ignored test: "+ res.getIgnoreCount());
-		System.out.println("Was successful? "+ res.wasSuccessful());
-*/
 		Result res = JUnitCore.runClasses(AbstractConstraintTestCouples.class);
 		 
 		for (Failure failure : res.getFailures()) {
