8aab8808f8516887962b51716ac92603544c884f
irene <irene.schimperna@gmail.com>
2020-06-22 16:40:21 +0200

    Aggiunti commenti con cose da fare

--- src/main/services/persistence/InterfaceUserDAO.java
+++ src/main/services/persistence/InterfaceUserDAO.java
@@ -5,6 +5,8 @@ import main.model.people.costumer.Customer;
 public interface InterfaceUserDAO {
 
 	public boolean addUsers(String name, String cognome, String email, String password, boolean isAdmin);
+	//TODO questo non dovrebbe prendere un oggetto Customer?
+	
 	public boolean login(String email, String password);
 	public Customer getCustomer(String email);
 	public boolean changeEmail(String oldEmail, String newEmail);
