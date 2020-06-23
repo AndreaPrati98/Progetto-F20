2de4a180e74fe8a450e9f4a6de69e901bb53c2f5
GarauCristian <cristian.garau01@universitadipavia.it>
2020-06-23 17:54:21 +0200

    Riaggiunta ridenominazione, rimosso footer

--- Website/JS/profile.js
+++ Website/JS/profile.js
@@ -22,6 +22,6 @@ function removeConfiguration(configurationId){
 }
 
 function renameConfiguration(configurationId){
-  let newName = $('textarea#' + configurationId).val();
+  let newName = $('input#' + configurationId).val();
   let posting = $.post( "/profile/rename", {id: configurationId, name: newName});
 }
