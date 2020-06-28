47bcfe0270717cabb0e1a3dc0e5114e5d321d1ae
AndreaPrati98 <62189267+AndreaPrati98@users.noreply.github.com>
2020-06-28 10:59:15 +0200

    aggiunta documentazione ConstraintCategory

--- src/main/model/configurator/constraint/ConstraintCategory.java
+++ src/main/model/configurator/constraint/ConstraintCategory.java
@@ -1,5 +1,12 @@
 package main.model.configurator.constraint;
 
+/**
+ * This {@link Enum} is used to distinct different category of Constraint, have a 
+ * look of the glossary
+ * 
+ * @author Alessandro Capici
+ *
+ */
 public enum ConstraintCategory {
 	INTERNAL,EXTERNAL
 }
