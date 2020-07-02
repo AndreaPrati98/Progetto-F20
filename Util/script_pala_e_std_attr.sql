938e3d087b12520dbe6a3f7dd4f301651e6f4d7d
gCass <guglielmo.cassini01@universitadipavia.it>
2020-07-02 15:05:34 +0200

    Messo a posto script sql

--- Util/script_pala_e_std_attr.sql
+++ Util/script_pala_e_std_attr.sql
@@ -1,7 +1,7 @@
 /*Esempio di query per inserimento di un nuovo tipo di componente e di uno StandardAttribute per 
 quel tipo di componente. Dopodichè si può usare l'interfaccia grafica del sito*/
 
-INSERT INTO TypeComponent VALUES ("pala eolica",0);
+INSERT INTO TypeComponent VALUES ("palaEolica",0);
 
 INSERT INTO StandardAttribute 
 VALUES ("cpuSocket","palaEolica","CpuSocket",NULL, 1);
